aa27efa0295137158cd8aaa0cd27b36cfc43097d
David Miguel de la Fuente <david.mdlf@gmail.com>
2016-06-14 22:39:12 +0200

    Dashboard with performance radar and grade timeline for groups

---
+++ classes/GroupData.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * GroupData class with static methods to retrieve learning analytics for student groups
+ *
+ * Author: David Miguel de la Fuente
+ */
+require_once($CFG->libdir.'/gradelib.php');
+require_once($CFG->dirroot .'/grade/querylib.php');
+
+class GroupData {
+
+    static function get_all_course_grades($group_id, $course_id){
+        global $DB;
+        $query = "SELECT AVG(((grade_grades.finalgrade - grade_items.grademin)/(grade_items.grademax - grade_items.grademin))*10) as grade, grade_items.itemname
+        FROM {grade_items} grade_items
+        JOIN {grade_grades} grade_grades ON grade_items.id = grade_grades.itemid
+        WHERE grade_items.itemtype = 'mod' AND grade_items.courseid = :course_id AND grade_grades.userid = ANY (
+        SELECT userid FROM {groups_members} groups_members JOIN {groups} groups ON groups.id = groups_members.groupid WHERE groups.courseid = :courseid AND groups.id = :group_id
+        )
+        GROUP BY itemname";
+        $records = $DB->get_records_sql($query, array("group_id" => $group_id, "course_id" => $course_id, "courseid" => $course_id));
+        $grades = array();
+        $labels = array();
+        foreach($records as $record){
+            $labels[] = "'".$record->itemname."'";
+            $grades[] = $record->grade;
+        }
+        return array(
+            'grades' => $grades,
+            'labels' => $labels
+        );
+    }
+
+    static function get_performance_radar($group_id, $course_id){
+        global $DB;
+        $query = "SELECT grade_items.itemmodule, (AVG(grade_grades.finalgrade)/AVG(grade_items.grademax)) as avggraderatio, grade_items.itemname
+        FROM {grade_items} grade_items
+        JOIN {grade_grades} grade_grades ON grade_items.id = grade_grades.itemid
+        WHERE grade_items.itemtype = 'mod' AND grade_items.courseid = :course_id AND grade_grades.userid = ANY (
+        SELECT userid FROM {groups_members} groups_members JOIN {groups} groups ON groups.id = groups_members.groupid WHERE groups.courseid = :courseid AND groups.id = :group_id
+        )
+        GROUP BY grade_items.itemmodule";
+        $records = $DB->get_records_sql($query, array("group_id" => $group_id, "course_id" => $course_id, "courseid" => $course_id));
+        $ratios = array();
+        $labels = array();
+        foreach($records as $record){
+            $labels[] = "'".$record->itemmodule."'";
+            $ratios[] = $record->avggraderatio;
+        }
+        return array(
+            'ratios' => $ratios,
+            'labels' => $labels
+        );
+    }
+}
---
+++ views/group_analytics.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Group analytics general template.
+ *
+ * Author: David Miguel de la Fuente
+ */
+require_once('classes/GroupData.php');
+
+
+if (!$group = $DB->get_record('groups', array('id' => required_param('group_id', PARAM_INT)), '*', MUST_EXIST)) {
+    print_error('nogroupid');
+}
+
+$PAGE->set_pagetype('course-view-' . $course->format);  // To get the blocks exactly like the course.
+$PAGE->add_body_class('path-user');                     // So we can style it independently.
+
+$PAGE->set_title(get_string('pluginname', "block_moodlean") . " | $course->fullname: $group->name");
+$PAGE->set_heading($course->fullname);
+$PAGE->set_pagelayout('standard');
+
+echo $OUTPUT->header();
+$all_course_grades = GroupData::get_all_course_grades($group->id, $course->id);
+$performance_radar = GroupData::get_performance_radar($group->id, $course->id);
+include "templates/dashboard.php";
+echo $OUTPUT->footer();
